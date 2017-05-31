c53fe60cd7f2ce1d742bb1c868f6c02bdede008b
RogerGrundmann <RogerGrundmann@users.noreply.github.com>
2017-05-31 10:16:01 +0200

    Add files via upload
    rewritten to match the new XML-config-files

--- cli/atm.cpp
+++ cli/atm.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
+#include <stdlib.h>
 
 #include "cAtmosphereModel.h"
 
 int main(int argc, char **argv) {
     cAtmosphereModel model;
 
-    if (argc != 2) {
-        std::cout << "ATOM atmosphere model\n";
-        std::cout << "\n";
-        std::cout << "Usage:\n";
-        std::cout << "\t" << argv[0] << " <XML configuration path>\n";
-        std::cout << "\n";
-        return 1;
-    }
+    if ( argc < 2 )
+    {
+        std::cout << endl;
+        std::cout << "missing command line parameters\n";
+        std::cout << endl;
+        exit ( 1 );
+	}
 
+    std::cout << endl;
+    std::cout << "ATOM atmosphere model\n";
+    std::cout << "\n";
+    std::cout << "Usage:\n";
+    std::cout << "\t" << argv[0] << "                      " << " <program name>\n";
+    std::cout << "\t" << argv[1] << "                  " << " <XML configuration path>\n";
+    std::cout << "\t" << argv[2] << "    " << " <XML file name>\n" << "\n";
+    std::cout << endl;
 
-    std::cout << "Loading configuration from " << argv[1] << "\n";
-
-    model.LoadConfig(argv[1]);
+    model.LoadConfig(argv[2]);
     model.Run();
 }
--- cli/hyd.cpp
+++ cli/hyd.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
+#include <stdlib.h>
 
 #include "cHydrosphereModel.h"
 
 int main(int argc, char **argv) {
     cHydrosphereModel model;
 
+    if ( argc < 2 )
+    {
+        std::cout << endl;
+        std::cout << "missing command line parameters\n";
+        std::cout << endl;
+        exit ( 1 );
+	}
+
+    std::cout << endl;
     std::cout << "ATOM hydrosphere model\n";
     std::cout << "\n";
     std::cout << "Usage:\n";
-    std::cout << "\t" << argv[0] << " [XML configuration path]\n";
-    std::cout << "If XML path is not specified, default parameters will be used\n";
-    std::cout << "\n";
+    std::cout << "\t" << argv[0] << "                      " << " <program name>\n";
+    std::cout << "\t" << argv[1] << "                  " << " <XML configuration path>\n";
+    std::cout << "\t" << argv[2] << "    " << " <XML file name>\n" << "\n";
+    std::cout << endl;
 
-    if (argc == 2) {
-        model.LoadConfig(argv[1]);
-    }
-
+    model.LoadConfig(argv[2]);
     model.Run();
 }
