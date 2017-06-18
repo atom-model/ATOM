47c8f108fd7cde98c7a73a070a5cf8af8e9d498f
RogerGrundmann <RogerGrundmann@users.noreply.github.com>
2017-06-18 10:21:53 +0200

    Add files via upload
    in libraries the 'c' dropped

--- python/setup.py
+++ python/setup.py
@@ -3,19 +3,18 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 ext_modules = [
-    Extension("pyatom",
+    Extension ( "pyatom",
               [
                   'pyatom.pyx',
                   'PythonStream.cpp'
               ],
-              language='c++',
-              libraries=['c', 'atom'],
-              include_dirs=['../atmosphere', '../hydrosphere', '../lib', '../tinyxml2'],
-              library_dirs=['..']
-              )]
+              language = 'c++',
+              libraries = [ 'atom' ],
+              include_dirs = [ '../atmosphere', '../hydrosphere', '../lib', '../tinyxml2' ],
+              library_dirs = [ '..' ]
+              ) ]
 
-setup(
-    name='pyatom',
-    cmdclass={'build_ext': build_ext},
-    ext_modules=ext_modules
-)
+setup (
+    name = 'pyatom',
+    cmdclass = { 'build_ext': build_ext },
+    ext_modules = ext_modules )
