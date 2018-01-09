96862cb6086d0a317b13db62c534c073a73f882e
michaelchin <michael.chin@sydney.edu.au>
2018-01-09 13:57:58 +1100

    Add a new class Int3DArray

--- lib/Array.h
+++ lib/Array.h
@@ -13,8 +13,30 @@
 #define _ARRAY_
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+class Int3DArray{
+    private:
+        int im, jm, km;
+
+    public:
+        std::vector<std::vector<std::vector<int> > > x;
+
+    Int3DArray(int idim, int jdim, int kdim, double val){
+        im=idim; jm=jdim; km=kdim;
+        x.resize(idim);
+        for(int i=0; i<idim; i++){
+            x[i].resize(jdim);
+            for(int j=0; j<jdim; j++){
+                 x[i][j].resize(kdim, val);
+            }
+        }
+    }
+
+    ~Int3DArray ( ){}
+};
 
 class Array
 {
@@ -29,5 +51,18 @@ public:
 
     void printArray ( int, int, int );
     void initArray ( int, int, int, double );
+
+    Int3DArray to_Int3DArray(){
+        Int3DArray a(im,jm,km,0);
+        for(int i=0; i<im; i++){
+            for(int j=0; j<jm; j++){
+                for(int k=0; k<km; k++){
+                    a.x[i][j][k]=int(round(x[i][j][k]));
+                }
+            }
+        }
+        return a;
+    }
+
 };
 #endif
