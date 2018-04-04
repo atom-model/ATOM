1eba7135267ac2eb4403f59204154f61c5d8168e
Michael Chin <mchin@Basin-Hub-MacBook-Pro.local>
2018-04-04 13:00:35 +1000

    add --allow-root in notebook.sh

--- docker/notebook.sh
+++ docker/notebook.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /workspace
-exec /usr/local/bin/jupyter notebook --no-browser --ip=0.0.0.0 --NotebookApp.token='' >>/var/log/notebook.log 2>&1
+exec /usr/local/bin/jupyter notebook --no-browser --ip=0.0.0.0 --NotebookApp.token='' --allow-root >>/var/log/notebook.log 2>&1
