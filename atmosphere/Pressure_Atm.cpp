1aa7ecd00cc6a9a7cb0b1010f1fced44ba137772
RogerGrundmann <roger.grundmann@web.de>
2018-08-07 10:45:02 +0200

    if statements revised

--- atmosphere/Pressure_Atm.cpp
+++ atmosphere/Pressure_Atm.cpp
@@ -349,24 +349,23 @@ void Pressure_Atm::computePressure_2D ( BC_Thermo &circulation, double u_0, doub
             if ( ( h.x[ 0 ][ j ][ k ] == 1. ) && ( h.x[ 0 ][ j+1 ][ k ] == 0. ) )
                         drhs_vdthe = ( aux_v.x[ 0 ][ j+1 ][ k ] - aux_v.x[ 0 ][ j ][ k ] ) / ( dthe * rm );
 
-            if ( ( h.x[ 0 ][ j ][ k ] == 1. ) && ( h.x[ 0 ][ j+1 ][ k ] == 0. ) && ( h.x[ 0 ][ j+2 ][ k ] == 0. ) )
+            if ( j < jm-2 )
+            {
+                if ( ( h.x[ 0 ][ j ][ k ] == 1. ) && ( h.x[ 0 ][ j+1 ][ k ] == 0. ) && ( h.x[ 0 ][ j+2 ][ k ] == 0. ) )
                         drhs_vdthe = ( - 3. * aux_v.x[ 0 ][ j ][ k ] + 4. * aux_v.x[ 0 ][ j+1 ][ k ] -
                         aux_v.x[ 0 ][ j+2 ][ k ] ) / ( 2. * dthe * rm );
-
-
-            if ( ( h.x[ 0 ][ j ][ k ] == 1. ) && ( h.x[ 0 ][ j-1 ][ k ] == 0. ) && ( h.x[ 0 ][ j-2 ][ k ] == 0. ) )
-                        drhs_vdthe = ( aux_v.x[ 0 ][ j - 1 ][ k ] - aux_v.x[ 0 ][ j ][ k ] ) / ( dthe * rm );
+            }
 
             if ( ( h.x[ 0 ][ j ][ k ] == 1. ) && ( h.x[ 0 ][ j-1 ][ k ] == 0. ) )
                         drhs_vdthe = ( - 3. * aux_v.x[ 0 ][ j ][ k ] + 4. * aux_v.x[ 0 ][ j-1 ][ k ] -
                         aux_v.x[ 0 ][ j-2 ][ k ] ) / ( 2. * dthe * rm );
 
-
-            if ( j == 1 )  drhs_vdthe = ( - 3. * aux_v.x[ 0 ][ j ][ k ] + 4. * aux_v.x[ 0 ][ j+1 ][ k ] -
-                                                        aux_v.x[ 0 ][ j+2 ][ k ] ) / ( 2. * dthe * rm );
-
-            if ( j == jm-2 )  drhs_vdthe = ( - 3. * aux_v.x[ 0 ][ j ][ k ] + 4. * aux_v.x[ 0 ][ j-1 ][ k ] -
-                                                        aux_v.x[ 0 ][ j-2 ][ k ] ) / ( 2. * dthe * rm );
+            if ( j >= 2 )
+            {
+            if ( ( h.x[ 0 ][ j ][ k ] == 1. ) && ( h.x[ 0 ][ j-1 ][ k ] == 0. ) && ( h.x[ 0 ][ j-2 ][ k ] == 0. ) )
+                        drhs_vdthe = ( - 3. * aux_v.x[ 0 ][ j ][ k ] + 4. * aux_v.x[ 0 ][ j-1 ][ k ] -
+                        aux_v.x[ 0 ][ j-2 ][ k ] ) / ( 2. * dthe * rm );
+            }
 
 
 // gradients of RHS terms at mountain sides 2.order accurate in phi-direction
@@ -375,31 +374,26 @@ void Pressure_Atm::computePressure_2D ( BC_Thermo &circulation, double u_0, doub
             if ( ( h.x[ 0 ][ j ][ k ] == 1. ) && ( h.x[ 0 ][ j ][ k+1 ] == 0. ) )
                         drhs_wdphi = ( aux_w.x[ 0 ][ j ][ k+1 ] - aux_w.x[ 0 ][ j ][ k ] ) / ( dphi * rmsinthe );
 
-            if ( ( h.x[ 0 ][ j ][ k ] == 1. ) && ( h.x[ 0 ][ j ][ k+1 ] == 0. ) && ( h.x[ 0 ][ j ][ k+2 ] == 0. ) )
-                        drhs_wdphi = ( - 3. * aux_w.x[ 0 ][ j ][ k ] + 4. * aux_w.x[ 0 ][ j ][ k+1 ] -
-                                                 aux_w.x[ 0 ][ j ][ k+2 ] ) / ( 2. * rmsinthe * dphi );
-
+            if ( k < km-2 )
+            {
+                if ( ( h.x[ 0 ][ j ][ k ] == 1. ) && ( h.x[ 0 ][ j ][ k+1 ] == 0. ) && ( h.x[ 0 ][ j ][ k+2 ] == 0. ) )
+                            drhs_wdphi = ( - 3. * aux_w.x[ 0 ][ j ][ k ] + 4. * aux_w.x[ 0 ][ j ][ k+1 ] -
+                                                     aux_w.x[ 0 ][ j ][ k+2 ] ) / ( 2. * rmsinthe * dphi );
+            }
 
             if ( ( h.x[ 0 ][ j ][ k ] == 0. ) && ( h.x[ 0 ][ j ][ k-1 ] == 1. ) )
                         drhs_wdphi = ( aux_w.x[ 0 ][ j ][ k-1 ] - aux_w.x[ 0 ][ j ][ k ] ) / ( dphi * rmsinthe );
 
-            if ( ( h.x[ 0 ][ j ][ k ] == 0. ) && ( h.x[ 0 ][ j ][ k-1 ] == 1. ) && ( h.x[ 0 ][ j ][ k-2 ] == 1. ) )
-                        drhs_wdphi = ( - 3. * aux_w.x[ 0 ][ j ][ k ] + 4. * aux_w.x[ 0 ][ j ][ k-1 ] -
-                                                 aux_w.x[ 0 ][ j ][ k-2 ] ) / ( 2. * rmsinthe * dphi );
-
-
-            if ( k == 2 )  drhs_wdphi = ( - 3. * aux_w.x[ 0 ][ j ][ k ] + 4. * aux_w.x[ 0 ][ j ][ k+1 ] -
-                                                             aux_w.x[ 0 ][ j ][ k+2 ] ) / ( 2. * rmsinthe * dphi );
-
-            if ( k == km-2 )  drhs_wdphi = ( - 3. * aux_w.x[ 0 ][ j ][ k ] + 4. * aux_w.x[ 0 ][ j ][ k-1 ] -
-                                                                 aux_w.x[ 0 ][ j ][ k-2 ] ) / ( 2. * rmsinthe * dphi );
-
+            if ( k >= 2 )
+            {
+                if ( ( h.x[ 0 ][ j ][ k ] == 0. ) && ( h.x[ 0 ][ j ][ k-1 ] == 1. ) && ( h.x[ 0 ][ j ][ k-2 ] == 1. ) )
+                            drhs_wdphi = ( - 3. * aux_w.x[ 0 ][ j ][ k ] + 4. * aux_w.x[ 0 ][ j ][ k-1 ] -
+                                                     aux_w.x[ 0 ][ j ][ k-2 ] ) / ( 2. * rmsinthe * dphi );
+            }
 
             p_dyn.x[ 0 ][ j ][ k ] = ( ( p_dynn.x[ 0 ][ j+1 ][ k ] + p_dynn.x[ 0 ][ j-1 ][ k ] ) * num2
                                                 + ( p_dynn.x[ 0 ][ j ][ k+1 ] + p_dynn.x[ 0 ][ j ][ k-1 ] ) * num3 
                                                 - r_air * ( drhs_vdthe + drhs_wdphi ) ) / denom;
-
-            if ( h.x[ 0 ][ j ][ k ] == 1. )                        p_dyn.x[ 0 ][ j ][ k ] = .0;
         }
     }
 
