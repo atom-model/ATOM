9bca967576b3c880d59becf8048243695065f33b
michaelchin <michael.chin@sydney.edu.au>
2019-01-23 19:54:04 +1100

    update param.py

--- param.py
+++ param.py
@@ -8,10 +8,10 @@ def main():
     # name, description, datatype, default
     PARAMS = {
         'common': [
-            ( 'bathymetry_path', '', 'string', '../data/Paleotopography_bathymetry/Golonka_rev210' ),
-            ( 'BathymetrySuffix', '', 'string', 'Ma_Golonka.xyz' ),
+            ( 'bathymetry_path', '', 'string', '../data/topo_grids' ),
+            ( 'BathymetrySuffix', '', 'string', 'Ma_smooth.xyz' ),
             ( 'verbose', '', 'bool', False ),
-            ( 'output_path', 'directory where model outputs should be placed ( must end in / )', 'string', 'output' ),
+            ( 'output_path', 'directory where model outputs should be placed ( must end in / )', 'string', 'output/' ),
             ( 'paraview_panorama_vts','flag to control if create paraview panorama', 'bool', False),
             ( 'debug','flag to control if the program is running in debug mode', 'bool', False),
         
